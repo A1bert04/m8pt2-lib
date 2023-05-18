88998f11315cf7b83a98fb668bfd847beed571f6
A1bert04 <albertbru04@gmail.com>
2023-05-18 10:41:06 +0200

    Tag

--- average.js
+++ average.js
@@ -1,4 +1,4 @@
-// Funcio per calcular la mitjana d'un array de numeros
+//  Funcio per calcular la mitjana d'un array de numeros
 export default function calcAverage(arr) {
     let sum = 0
     arr.forEach((item) => {
